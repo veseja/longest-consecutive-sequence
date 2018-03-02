05e65333c2d7b57008ff9ee7e4565c2768506279
Liubou Shchupak <36640822+veseja@users.noreply.github.com>
2018-03-02 16:13:26 +0300

    onlogn:(

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+
+    if (array.length == 0) return 0;
+    function compareNumeric(a, b)
+    {
+        return a - b;
+    }
+    array.sort(compareNumeric);
+    
+    var longestSequence = 0;
+    var currentSequence = 1;
+
+    for (var i = 0; i < array.length; i++)
+    {
+        if (array[i] === array[i-1]) continue;
+        if (array[i] === array[i-1] + 1) currentSequence++;
+        else {
+            if (longestSequence < currentSequence) longestSequence = currentSequence;
+            currentSequence = 1;
+        }
+    }
+    if (longestSequence < currentSequence) longestSequence = currentSequence;
+    return longestSequence;
 }
